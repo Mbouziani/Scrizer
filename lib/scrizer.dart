403fa112002d5e2d045cc8874b45b6c5724be65b
Bouziani Mohammed <bouzimoha150@gmail.com>
2025-04-15 17:41:49 +0100

    add the new class exports

--- lib/scrizer.dart
+++ lib/scrizer.dart
@@ -1,6 +1,13 @@
 library;
 
+export 'src/bloc/scrizer_bloc_state.dart';
+export 'src/enums/data_state_enum.dart';
 export 'src/enums/device_type.dart';
+export 'src/errors/failure.dart';
+export 'src/extensions/color_extension.dart';
+export 'src/extensions/route_extension.dart';
 export 'src/scrizer.dart';
+export 'src/themes/theme_colors.dart';
+export 'src/usecase/usecase.dart';
 export 'src/widgets/scrizer_layout.dart';
 export 'src/widgets/scrizer_text.dart';
